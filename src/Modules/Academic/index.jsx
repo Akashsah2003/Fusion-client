0891d0f9256cd817369c5c7e7200a6a151689281
nihal6674 <nmohammaedali@gmail.com>
2025-02-17 17:35:46 +0530

    added verify student registration

--- src/Modules/Academic/index.jsx
+++ src/Modules/Academic/index.jsx
@@ -14,6 +14,7 @@ import AcademicCalendar from "./AcademicCalendar";
 import GenerateStudentList from "./GenerateStudentList";
 import ViewRollList from "./ViewRollList";
 import AllocateCourses from "./AllocateCourses";
+import VerifyStudentRegistration from "./VerifyStudentRegistration";
 
 function AcademicPage() {
   const [activeTab, setActiveTab] = useState("0");
@@ -29,6 +30,7 @@ function AcademicPage() {
       { title: "Academic Calendar" },
       { title: "Generate Student List" },
       { title: "Allocate Courses" },
+      { title: "Verify Student Registration" },
     ];
     tabComponents = [
       StudentCourses,
@@ -36,6 +38,7 @@ function AcademicPage() {
       AcademicCalendar,
       GenerateStudentList,
       AllocateCourses,
+      VerifyStudentRegistration,
     ];
   } else if (role === "student") {
     tabItems = [
